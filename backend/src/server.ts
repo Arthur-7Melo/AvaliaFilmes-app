d3e80de9ee35ddd76ac420fdbe441786d57c40cc
Arthur Melo <arthur7m@gmail.com>
2025-06-01 18:03:39 -0300

    docs(auth): Adiciona documentação swagger para endpoints de autenticação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,23 @@ import dotenv from 'dotenv';
 import { connectDB } from './db/db';
 import authRouter from './routes/authRouter';
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
 const app = express();
 
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+app.use(
+  '/api/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
