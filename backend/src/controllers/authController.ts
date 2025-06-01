ccdd295167c1b8e17171dd2e080f8e5210b5656c
Arthur Melo <arthur7m@gmail.com>
2025-06-01 16:44:29 -0300

    feat(auth): Implementa lógica de reset-password

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { createUser, forgotPassword, login } from "../services/authService";
-import { CreateUserInput, ForgotPasswordInput, SignInInput } from "../db/schemas/userSchema";
+import { createUser, forgotPassword, login, resetPassword } from "../services/authService";
+import { CreateUserInput, ForgotPasswordInput, ResetPasswordInput, SignInInput } from "../db/schemas/userSchema";
 import { BadRequestError, ConflictError, NotFoundError } from "../utils/customError";
 
 export const signup = async (req: Request, res: Response) => {
@@ -66,6 +66,32 @@ export const forgotPasswordHandler = async (req: Request, res: Response) => {
       });
     } else {
       console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor"
+      });
+    }
+  }
+}
+
+export const resetPasswordHandler = async (req: Request, res: Response) => {
+  try {
+    const resetToken = req.params.token;
+    const { password }: ResetPasswordInput = req.body;
+
+    await resetPassword(resetToken, password);
+
+    res.status(200).json({
+      success: true,
+      message: "Senha alterada com sucesso"
+    })
+  } catch (error) {
+    if (error instanceof BadRequestError) {
+      res.status(error.statusCode).json({
+        success: false,
+        message: error.message
+      });
+    } else {
       res.status(500).json({
         success: false,
         message: "Erro interno do servidor"
