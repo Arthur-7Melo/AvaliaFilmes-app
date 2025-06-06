46c49822466b384f195dee473188680cc0841319
Arthur Melo <arthur7m@gmail.com>
2025-06-06 12:24:55 -0300

    feat(tmdb): Adiciona tmdbService com axios

---
+++ backend/src/services/tmdbService.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { NotFoundError } from "../utils/customError";
+
+const TMDB_KEY = process.env.TMDB_API_KEY;
+const TMDB_URL = process.env.TMDB_URL;
+
+export const tmdb = axios.create({
+  baseURL: TMDB_URL,
+  params: {
+    api_key: TMDB_KEY,
+    language: "pt-BR"
+  }
+});
+
+export const getGenres = async () => {
+  const response = await tmdb.get("/genre/movie/list");
+  return response.data.genres;
+}
+
+export const discoverMovies = async ({
+  genreId,
+  releaseYear,
+  page = 1,
+}: {
+  genreId?: number,
+  releaseYear?: number,
+  page?: number,
+}) => {
+  const params: Record<string, any> = { page };
+  if (genreId) params.with_genres = genreId;
+  if (releaseYear) params.primary_release_year = releaseYear;
+
+  const response = await tmdb.get("/discover/movie", { params });
+  return response.data;
+}
+
+export const getMovieDetails = async (movieId: number) => {
+  const response = await tmdb.get(`/movie/${movieId}`);
+  return response.data;
+}
+
+export const searchMovie = async ({
+  query,
+  page = 1,
+}: {
+  query: string,
+  page?: number,
+}) => {
+  const response = await tmdb.get("/search/movie", {
+    params: { query, page }
+  });
+  return response.data;
+}
